9130df50607ae589f48e1006b5e27fe471b526f1
SoyNil <1422624@senati.pe>
2023-09-21 16:26:17 -0500

    Calculado final

--- Calculadora/Calculadora/MainPage.xaml.cs
+++ Calculadora/Calculadora/MainPage.xaml.cs
@@ -10,13 +10,130 @@ namespace Calculadora
 {
     public partial class MainPage : ContentPage
     {
+        private string currentInput = ""; // Variable para almacenar la entrada actual
+        private double result = 0; // Variable para almacenar el resultado
+        private string currentOperator = ""; // Variable para rastrear el operador actual
+        private double firstNumber = double.NaN; // Inicializa con un valor nulo
+
         public MainPage()
         {
             InitializeComponent();
         }
-        private void Btn_sumar(Object sender, EventArgs e)
-        {
+        private bool isResultDisplayed = false;
 
+        // ...
+
+        private void UpdateUI()
+        {
+            if (!string.IsNullOrEmpty(currentInput) || !double.IsNaN(firstNumber))
+            {
+                if (isResultDisplayed)
+                {
+                    spnFirst.Text = "";
+                    spnSecond.Text = "";
+                    spnThird.Text = "";
+                }
+                else
+                {
+                    spnFirst.Text = double.IsNaN(firstNumber) ? "" : firstNumber.ToString();
+                    spnSecond.Text = currentOperator;
+                    spnThird.Text = currentInput;
+                }
+            }
+            else
+            {
+                spnFirst.Text = "";
+                spnSecond.Text = "";
+                spnThird.Text = "";
+            }
+
+            lblNumber.Text = isResultDisplayed ? result.ToString() : currentInput;
+        }
+
+        // ...
+
+        private void Btn_sumar(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string buttonText = button.Text;
+
+            switch (buttonText)
+            {
+                case "AC":
+                    // Limpiar todo
+                    currentInput = "";
+                    firstNumber = double.NaN;
+                    currentOperator = "";
+                    isResultDisplayed = false; // Restablecer la bandera
+                    break;
+                case "=":
+                    // Realizar la operación y mostrar el resultado
+                    if (!string.IsNullOrEmpty(currentInput) && !string.IsNullOrEmpty(currentOperator))
+                    {
+                        double num;
+                        if (double.TryParse(currentInput, out num))
+                        {
+                            switch (currentOperator)
+                            {
+                                case "+":
+                                    result = firstNumber + num;
+                                    break;
+                                case "-":
+                                    result = firstNumber - num;
+                                    break;
+                                case "*":
+                                    result = firstNumber * num;
+                                    break;
+                                case "/":
+                                    if (num != 0)
+                                    {
+                                        result = firstNumber / num;
+                                    }
+                                    else
+                                    {
+                                        // Manejar la división por cero aquí
+                                    }
+                                    break;
+                            }
+
+                            currentInput = result.ToString();
+                            firstNumber = result;
+                            currentOperator = "";
+                            isResultDisplayed = true; // Establecer la bandera
+                        }
+                    }
+                    break;
+                case "←":
+                    // Eliminar el último carácter
+                    if (!string.IsNullOrEmpty(currentInput))
+                    {
+                        currentInput = currentInput.Remove(currentInput.Length - 1);
+                    }
+                    break;
+                default:
+                    // Verificar si es un operador
+                    if (buttonText == "+" || buttonText == "-" || buttonText == "*" || buttonText == "/")
+                    {
+                        // Almacena el primer número y el operador
+                        if (!string.IsNullOrEmpty(currentInput))
+                        {
+                            firstNumber = double.Parse(currentInput);
+                        }
+                        currentOperator = buttonText;
+                        currentInput = "";
+                        isResultDisplayed = false; // Restablecer la bandera
+                    }
+                    else
+                    {
+                        // Agregar el número a la entrada actual
+                        currentInput += buttonText;
+                    }
+                    break;
+            }
+
+            // Actualiza la interfaz de usuario
+            UpdateUI();
         }
     }
 }
+
